2168abeb5f666c7019fc3693480bc1668c509f75
Michael <tharratt.michael@gmail.com>
2022-10-26 11:40:47 -0700

    Created JS to toggle class and remove active notifications

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ const profileNameBtn = document.querySelectorAll('#profileName');
 const postTitleBtn = document.querySelectorAll('#postTitle');
 const groupTitleBtn = document.querySelectorAll('#groupTitle');
 
-let notificationContainers = document.getElementsByClassName('notification-container');
+const notificationContainers = document.querySelectorAll('.notification-container')
+const notiNumber = document.getElementById('notiNumber')
 
-markReadBtn.addEventListener("click", e => {
-    notificationContainers.forEach(c)
-});
+
+notificationContainers.forEach((btn) => {
+    markReadBtn.addEventListener("click", (e) => {
+        notificationContainers.forEach(f => f.classList.remove('active'));
+        notiNumber.innerText = '0'
+    });
+  });
