dc4ee92b65bdf5212165e8ddf140fdbba6e9e36c
Michael <tharratt.michael@gmail.com>
2022-10-26 12:10:23 -0700

    Added red dots to unread notifications

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 const markReadBtn = document.getElementById('markRead');
-const profileNameBtn = document.querySelectorAll('#profileName');
-const postTitleBtn = document.querySelectorAll('#postTitle');
-const groupTitleBtn = document.querySelectorAll('#groupTitle');
-
 const notificationContainers = document.querySelectorAll('.notification-container')
 const notiNumber = document.getElementById('notiNumber')
+const notifiDots = document.querySelectorAll('.dot');
 
 
 notificationContainers.forEach((btn) => {
     markReadBtn.addEventListener("click", (e) => {
         notificationContainers.forEach(f => f.classList.remove('active'));
         notiNumber.innerText = '0'
+        notifiDots.forEach(dot => dot.classList.remove('active'));
     });
   });
